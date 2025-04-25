f77a6336c64bec880ee1f1a61226ba96a3a59785
tien198 <tienvn998@gmail.com>
2025-04-25 21:11:26 +0700

    add enum to Type Model schema

--- models/Type.js
+++ models/Type.js
@@ -1,7 +1,10 @@
 import Mogoose, { Schema } from 'mongoose'
 
 const typeSchema = new Schema({
-    name: { type: String, required: true },
+    name: {
+        type: String, enum: ['Apartments', 'Resorts', 'Cabins', 'Hotels', 'Villas'],
+        required: true
+    },
     count: { type: Number },
     image: { type: String }
 })
